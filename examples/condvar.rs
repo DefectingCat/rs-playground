133d9e7aa7f868e66235caee50865de911e209e2
DefectingCat <i@defect.ink>
2023-10-13 14:36:16 +0800

    init with examples
    add condvar

---
+++ examples/condvar.rs
@@ -0,0 +1,40 @@
+use std::{
+    sync::{Arc, Condvar, Mutex},
+    thread,
+    time::Duration,
+};
+
+fn main() {
+    let cpus = thread::available_parallelism().unwrap().get();
+
+    let flag = Arc::new(Mutex::new(false));
+    let cond = Arc::new(Condvar::new());
+
+    let cflag = flag.clone();
+    let ccond = cond.clone();
+    let handler = thread::spawn(move || {
+        let mut lock = cflag.lock().unwrap();
+        let mut counter = 0;
+
+        while counter < 3 {
+            while !*lock {
+                lock = ccond.wait(lock).unwrap();
+            }
+
+            *lock = false;
+            counter += 1;
+            println!("Child counter: {}", counter);
+        }
+    });
+
+    let mut counter = 0;
+    while counter < 3 {
+        thread::sleep(Duration::from_millis(1000));
+        *flag.lock().unwrap() = true;
+        counter += 1;
+        println!("Parent counter: {}", counter);
+        cond.notify_one();
+    }
+
+    handler.join().unwrap();
+}
