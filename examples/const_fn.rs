022d40ef79b30ed424c3101a2d0f429bee464970
DefectingCat <i@defect.ink>
2023-12-31 23:51:56 +0800

    update const fn example

--- examples/const_fn.rs
+++ examples/const_fn.rs
@@ -1,31 +1,50 @@
-const fn five_numbers() -> [i32; 5] {
-    let mut numbers = [0i32; 5];
+const fn five_numbers() -> [u32; 5] {
+    let mut numbers = [0u32; 5];
 
-    // for (n, i) in numbers.iter().enumerate() {}
-    let mut i = 0_i32;
+    let mut i = 0;
     while i < 5 {
-        numbers[i as usize] = i + 1;
+        numbers[i] = i as u32 + 1;
         i += 1;
     }
 
     numbers
 }
-const fn numbers<const N: usize>() -> [i32; N] {
-    let mut numbers = [0i32; N];
+
+const fn numbers<const N: usize>() -> [u32; N] {
+    let mut numbers = [0u32; N];
 
     let mut i = 0;
     while i < N {
-        numbers[i] = i as i32 + 1;
+        numbers[i] = i as u32 + 1;
         i += 1;
     }
 
     numbers
+}
+
+const fn len(strs: &[&str]) -> usize {
+    let mut result = 0;
+    let mut remaining = strs;
+
+    // get first element in array, calculate its length
+    // then use slice deconstruction to update remaining
+    // until empty
+    while let [current, tail @ ..] = remaining {
+        result += current.len();
+        remaining = tail;
+    }
+
+    result
 }
 
 fn main() {
     let five_numbers = five_numbers();
-    println!("{:?}", five_numbers);
+    println!("five numerbs {:?}", five_numbers);
 
-    let numbers: [i32; 10] = numbers();
-    println!("{:?}", numbers);
+    let numbers: [u32; 10] = numbers();
+    println!("generic numbers {:?}", numbers);
+
+    let strs = ["xfy", "dfy", "wudixiaofeiyang"];
+    let str_len = len(&strs);
+    println!("strs length {}", str_len);
 }
