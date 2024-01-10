2aee731603959f508b3c11b4a2bec36a419a236d
DefectingCat <i@defect.ink>
2024-01-10 13:23:52 +0800

    update select

--- examples/tokio_select.rs
+++ examples/tokio_select.rs
@@ -1,25 +1,24 @@
 use anyhow::Result;
-use tokio::sync::oneshot;
+use tokio::{
+    sync::oneshot,
+    time::{sleep, Duration},
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let (tx1, rx1) = oneshot::channel();
-    let (tx2, rx2) = oneshot::channel();
 
     tokio::spawn(async {
+        sleep(Duration::from_millis(99)).await;
         let _ = tx1.send("one");
-    });
-
-    tokio::spawn(async {
-        let _ = tx2.send("two");
     });
 
     tokio::select! {
         val = rx1 => {
-            println!("rx1 completed first with {:?}", val);
+            println!("rx1 completed first with {:?}", val?);
         }
-        val = rx2 => {
-            println!("rx2 completed first with {:?}", val);
+        _ = sleep(Duration::from_millis(100)) => {
+            println!("sleep from 100 milliseconds");
         }
     }
 
